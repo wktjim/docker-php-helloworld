d82d83e1c422306e3a65f1b91e7b073c9a0f9afd
root <wktjim@hotmail.com>
2021-07-30 16:50:33 +0800

    v2 :wq

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "v1 Hello, World from Docker! <br>";
+echo "v2 Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
